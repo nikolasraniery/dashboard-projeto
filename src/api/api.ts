b28cfd618acea12882c1de4189947b3de221bb79
nikolasraniery <nikolassraniery@gmail.com>
2024-01-15 16:01:53 -0300

    Alter Route Api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://65a57eff5a5ead21e3530bd7--fabulous-genie-edd6f2.netlify.app/data.json'
+    baseURL: 'https://dashboard-time-tracking-site.netlify.app/data.json'
 })
