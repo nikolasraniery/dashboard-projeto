f433971f73dc1bce1e8a8186332896b641eecfd1
nikolasraniery <nikolassraniery@gmail.com>
2024-01-15 16:51:59 -0300

    Settings api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://dashboard-time-tracking-site.netlify.app/data.json'
-})
+    baseURL: 'https://dashboard-time-tracking-site.netlify.app/',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    },
+});
