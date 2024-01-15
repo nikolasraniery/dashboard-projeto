a1683cfb00bf08234efa0d1357bbb087ef7503ce
nikolasraniery <nikolassraniery@gmail.com>
2024-01-15 15:57:51 -0300

    Adding Route Api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://65a57eff5a5ead21e3530bd7--fabulous-genie-edd6f2.netlify.app/data.json'
 })
